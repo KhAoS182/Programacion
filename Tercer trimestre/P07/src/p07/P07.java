49a0cc0ce39e09938031e1e7c684fb826f413d80
iperfectfurius <iperfectufurius@hotmail.com>
2019-03-21 14:47:20 +0100

    p07

---
+++ Tercer trimestre/P07/src/p07/P07.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p07;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Adrian
+ */
+public class P07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	
+    }
+    
+}
