ee4b577f92e736daa555e8c41694e5d4e20aeda2
iperfectfurius <iperfectufurius@hotmail.com>
2019-03-21 22:50:21 +0100

    p07

--- Tercer trimestre/P07/src/p07/P07.java
+++ Tercer trimestre/P07/src/p07/P07.java
@@ -6,7 +6,11 @@
 package p07;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
+
 
 /**
  *
@@ -18,8 +22,57 @@ public class P07 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+	int i;
+	boolean peli = true;
+	FileInputStream fin;
 	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	
+	if (args.length != 1) {
+	    System.out.println("Uso: MostrarArchivo.");
+	}
+	System.out.println("----------------------------------------------");
+	System.out.println("	     Cartelera cinefbmoll");
+	System.out.println("----------------------------------------------");
+	try {
+	//Abrir el archivo
+	    fin = new FileInputStream("C:\\Users\\Adrian\\Desktop\\Fp\\Grado Superior\\Programación\\Programacion Git-hub\\Tercer trimestre\\P07\\ejemplo.txt");
+	} catch (FileNotFoundException exc) {
+	    System.out.println("Archivo no encontrado");
+	    return;
+	}
+	try {
+	    //Leer bytes hasta que se encuentre el EOF
+	    //EOF es un concepto para determinar el final de un archivo
+	    do {
+		int j=0;
+		i = fin.read();
+		char x = (char) i;
+		if (i != -1) {
+		    if (x == '#'){
+			System.out.println();
+			
+			
+		    }
+		    else if (x == '{'){
+			System.out.print("\n----------");
+		    }
+		    else
+		    System.out.print((char) i);
+
+		    
+		}
+
+
+	    } while (i != -1); //Cuando i es igual a -1, se ha alcanzado el final del archivo
+	} catch (IOException exc) {
+	    System.out.println("Error al leer el archivo");
+	}
+	try {
+	    fin.close();
+//Cerrar el archivo
+	} catch (IOException exc) {
+	    System.out.println("Error cerrando el archivo.");
+	}
     }
-    
 }
+
+
