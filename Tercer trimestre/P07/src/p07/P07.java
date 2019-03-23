f18dc4d32a36935ceb218ff7135a6f44172cdc95
iperfectfurius <iperfectufurius@hotmail.com>
2019-03-23 03:57:15 +0100

    Update P07.java

--- Tercer trimestre/P07/src/p07/P07.java
+++ Tercer trimestre/P07/src/p07/P07.java
@@ -6,11 +6,12 @@
 package p07;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 /**
@@ -22,11 +23,39 @@ public class P07 {
     static Scanner sc = new Scanner(System.in);
     static String datos[] = {"Año: ", "Director: ", "Duracion: ", "Sinopsis: ", "Reparto:", "Sesión: "};
     static int ndatos = 0;
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-	cargarfichero();
+    public static void main(String[] args) throws IOException {
+	System.out.println("¿Como quieres leer el fichero?");
+	boolean salir = true;
+	while (salir) {
+	    switch (menu()) {
+		case 1:
+		    cargarficherobtb();
+		    salir = false;
+		    break;
+		case 2:
+		    cargarficherocac();
+		    break;
+		case 3:
+		    break;
+		default:
+		    System.out.println("Introduce una opción valida....");
+	    }
+	}
+    }
+
+    public static int menu() {
+	System.out.println("--------------------------");
+	System.out.println("1) Byte to Byte");
+	System.out.println("2) Caracter a caracter");
+	System.out.println("3) Línia a línia");
+	System.out.println("--------------------------");
+	int x = sc.nextInt();
+	sc.nextLine();// :)
+	return x;
     }
 
     public static void menuCartelera() {
@@ -34,9 +63,9 @@ public class P07 {
 	System.out.println("	     Cartelera cinefbmoll");
 	System.out.println("----------------------------------------------");
     }
-    public static void cargarfichero(){
+
+    public static void cargarficherobtb() {
 	int i;
-	boolean peli = true;
 	FileInputStream fin;
 	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	/*if (args.length != 1) {
@@ -44,9 +73,8 @@ public class P07 {
 	}*/
 	try {
 	    //Abrir el archivo
-	    System.out.println("Donde esta el archivo....."); // .\.\ejemplo.txt funciona & .\ejemplo.txt funciona & ejemplo.txt tambien??? pum cervell
-	    String ruta = sc.nextLine();
-	    fin = new FileInputStream(ruta);
+	    System.out.println("Indica la ruta del archivo....."); // .\.\ejemplo.txt funciona & .\ejemplo.txt funciona & ejemplo.txt tambien??? pum cervell
+	    fin = new FileInputStream(sc.nextLine());
 	    // fin = new FileInputStream("C:\\Users\\Adrian\\Desktop\\Fp\\Grado Superior\\Programación\\Programacion Git-hub\\Tercer trimestre\\P07\\ejemplo.txt");
 	} catch (FileNotFoundException exc) {
 	    System.out.println("Archivo no encontrado");
@@ -75,16 +103,55 @@ public class P07 {
 		    } else {
 			System.out.print((char) i);
 		    }
-		}		
+		}
 	    } while (i != -1); //Cuando i es igual a -1, se ha alcanzado el final del archivo
 	} catch (IOException exc) {
 	    System.out.println("Error al leer el archivo");
 	}
 	try {
 	    fin.close();
+	    
 //Cerrar el archivo
 	} catch (IOException exc) {
 	    System.out.println("Error cerrando el archivo.");
 	}
     }
+
+    public static void cargarficherocac() throws IOException {
+	// Define el archivo a usar
+	System.out.println("Introduce la ruta del archivo....");
+	File archivoEntrada = new File(sc.nextLine());
+	// Declara una variable que contendrá el caracter a leer
+	try ( // Instancia un FileReader que se encargara de leer del archivo
+		FileReader lector = new FileReader(archivoEntrada)) {
+	    // Declara una variable que contendrá el caracter a leer
+	    int unCaracter;
+	    System.out.print("----------");
+	    // Lee el archivo e informa
+	    while ((unCaracter = lector.read()) != -1) {
+		char x = (char) unCaracter;
+		if (x == '#') {
+			if (ndatos == 0) {
+			    System.out.println("----------");
+
+			}
+			System.out.print("\n" + datos[ndatos]);
+			ndatos++;
+
+		    } else if (x == '{') {
+			System.out.print("\n\n----------");
+			ndatos = 0;
+		    }
+		    else{
+		System.out.print((char) unCaracter);
+		    }
+	    }
+	    // Cierra el archivo
+	    lector.close();
+	    System.out.println();
+	}
+	catch(IOException e){
+	    System.out.println("Ha ocurrido algun error...");  
+    }
+}
 }
