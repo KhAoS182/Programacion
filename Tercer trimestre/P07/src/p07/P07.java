e42428a258fa93ce021f47b307b163c4830a7d58
iperfectfurius <iperfectufurius@hotmail.com>
2019-03-25 04:02:50 +0100

    P07 Streams

--- Tercer trimestre/P07/src/p07/P07.java
+++ Tercer trimestre/P07/src/p07/P07.java
@@ -6,6 +6,7 @@
 package p07;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -43,6 +44,7 @@ public class P07 {
 		    cargarficherocac();
 		    break;
 		case 3:
+		    cargarficherolal();
 		    break;
 		case 4:
 		    salir = false;
@@ -58,9 +60,10 @@ public class P07 {
 	System.out.println("1) Byte to Byte");
 	System.out.println("2) Caracter a caracter");
 	System.out.println("3) Línia a línia");
+	System.out.println("4) Salir");
 	System.out.println("--------------------------");
-	System.out.println("Indica la ruta del archivo a leer y su salida....");
 	int x = sc.nextInt();
+	System.out.println("Indica la ruta del archivo a leer y su salida....");
 	sc.nextLine();// :)
 	return x;
     }
@@ -72,6 +75,7 @@ public class P07 {
     }
 
     public static void cargarficherobtb(String rutain, String rutaout) throws FileNotFoundException {
+	ndatos = 0;
 	String saltolinia = "\n";
 	String formatpelicula = "----------";
 	String formatpelicula2 = "\n\n----------";
@@ -80,7 +84,7 @@ public class P07 {
 	byte[] formatpeliculabytes2 = formatpelicula2.getBytes(StandardCharsets.UTF_8);
 	int i;
 	FileInputStream fin = new FileInputStream(rutain);
-	FileOutputStream fout = new FileOutputStream(rutaout);
+	FileOutputStream fout = new FileOutputStream(rutaout, true);
 	//Primero, asegúrese de que ambos archivos hayan sido especificados.
 	//Copiar un Archivo
 	try {
@@ -136,13 +140,12 @@ public class P07 {
     }
 
     public static void cargarficherocac() throws IOException {
+	ndatos = 0;
 	String saltolinia = "\n";
 	String formatpelicula = "----------";
 	String formatpelicula2 = "\n\n----------";
 	// Define el archivo a usar
-	System.out.println("Introduce la ruta del archivo....");
-	File archivoEntrada = new File(sc.next());
-	File archivoSalida = new File(sc.next());
+	File archivoEntrada = new File(sc.next()); File archivoSalida = new File(sc.next());	
 	//instanciamos un writer
 	FileWriter writer = new FileWriter(archivoSalida);
 	// Declara una variable que contendrá el caracter a leer
@@ -183,4 +186,75 @@ public class P07 {
 	    System.out.println("Ha ocurrido algun error...");
 	}
     }
-}
+
+    public static void cargarficherolal() throws IOException {
+	ndatos = 0;
+	String saltolinia = "\n";
+	String formatpelicula = "----------";
+	String formatpelicula2 = "\n\n----------";
+	// Instancia un objeto File de entrada
+	File archivoEntrada = new File(sc.next());
+	File archivoSalida = new File(sc.next());
+	// Construye un BufferedReader
+	BufferedReader reader = new BufferedReader(new FileReader(archivoEntrada));
+	BufferedWriter writer = new BufferedWriter(new FileWriter(archivoSalida));
+	// Defino variables
+	boolean eof = false;
+	String lineaLeida = "";
+	String[] broken_text = null;
+	// Lee el archivoEntrada de forma eficiente e imprime los datos en pantalla
+	while (!eof) {
+	    // Lee una linea entera
+	    lineaLeida = reader.readLine();//una vez leída la línea la podemos partir con el método split, suele ser muy útil
+	    // Imprime la linea en pantalla
+	    if (lineaLeida != null) {
+		broken_text = lineaLeida.split(" ");
+	    } // Si llego al final del archivo, termina la ejecución
+	    else {
+		eof = true;
+	    }
+	}
+	    System.out.print(formatpelicula);
+	    try{
+		writer.write(formatpelicula);
+	    for (int i = 0; i < broken_text.length; i++) {
+		for (int j = 0; j < broken_text[i].length(); j++) {
+			// Declara una variable que contendrá el caracter a leer
+			// Lee el archivo e informa
+			char x = broken_text[i].charAt(j);
+			if (x == '#') {
+			    if (ndatos == 0) {
+				System.out.println("----------");
+				writer.write(formatpelicula);
+			    }
+			    System.out.print("\n" + datos[ndatos]);
+			    writer.write(saltolinia);
+			    writer.write(datos[ndatos]);
+			    ndatos++;
+
+			} else if (x == '{') {
+			    System.out.print("\n\n----------");
+			    writer.write(formatpelicula2);
+			    ndatos = 0;
+			} else {
+			    if (j == 0 && i!=0 ) {
+				System.out.print(" " + x);
+				writer.write(" " + x);
+			    } else {
+				System.out.print(x);
+				writer.write(x);
+			    }
+			}	    
+		}
+	    }
+		    } catch (Exception e) {
+
+		}
+
+	 // Cierra el FileReader
+	System.out.println();
+    reader.close();
+    writer.close();
+	    }
+    }
+	
