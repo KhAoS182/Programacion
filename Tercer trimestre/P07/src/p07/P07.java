c8e78716b0867121222f210278e71a9eb09c5c83
iperfectfurius <iperfectufurius@hotmail.com>
2019-04-03 04:25:19 +0200

    btb exceptions

--- Tercer trimestre/P07/src/p07/P07.java
+++ Tercer trimestre/P07/src/p07/P07.java
@@ -15,12 +15,15 @@ import java.io.IOException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.InvalidPathException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Date;
 import java.util.Scanner;
 import java.time.format.DateTimeFormatter;
+import java.util.InputMismatchException;
 
 /**
  *
@@ -31,36 +34,43 @@ public class P07 {
     static Scanner sc = new Scanner(System.in);
     static String datos[] = {"Año: ", "Director: ", "Duracion: ", "Sinopsis: ", "Reparto: ", "Sesión: "};
     static int ndatos = 0;
-    static Date mydate = new Date();
-    static LocalTime time = LocalTime.now();
-    static String log = "log.log";//me gusta mas
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
 
-	System.out.println("¿Como quieres leer el fichero?");
 	boolean salir = true;
 	while (salir) {
-	    switch (menu()) {
-		case 1:
-		    cargarficherobtb(sc.next(), sc.next());
+	    try {
+		switch (menu()) {
+		    case 1:
+			System.out.println("Indica la ruta del archivo a leer y su salida....");
+			cargarficherobtb();
 
-		    break;
-		case 2:
-		    cargarficherocac();
-		    break;
-		case 3:
-		    cargarficherolal();
-		    break;
-		case 4:
-		    salir = false;
-		    break;
-		default:
-		    System.out.println("Introduce una opción valida....");
+			break;
+		    case 2:
+			System.out.println("Indica la ruta del archivo a leer y su salida....");
+			cargarficherocac();
+			break;
+		    case 3:
+			System.out.println("Indica la ruta del archivo a leer y su salida....");
+			cargarficherolal();
+			break;
+		    case 4:
+			salir = false;
+			break;
+		    default:
+			System.out.println("Introduce una opción valida....");
+			imprimirlog("No se han introducido datos válidos");
+		}
+	    } catch (InputMismatchException e) {
+		System.out.println("Introduce datos validos. " + "(" + e + ")");
+		imprimirlog("No se han introducido datos válidos" + "(" + e + ")");
+		sc.nextLine();
 	    }
 	}
+
     }
 
     public static int menu() {
@@ -71,7 +81,6 @@ public class P07 {
 	System.out.println("4) Salir");
 	System.out.println("--------------------------");
 	int x = sc.nextInt();
-	System.out.println("Indica la ruta del archivo a leer y su salida....");
 	sc.nextLine();// :)
 	return x;
     }
@@ -82,78 +91,72 @@ public class P07 {
 	System.out.println("----------------------------------------------");
     }
 
-    public static void cargarficherobtb(String rutain, String rutaout) throws FileNotFoundException {
+    public static void cargarficherobtb() throws FileNotFoundException, InvalidPathException, IOException {
 	ndatos = 0;
-	String saltolinia = "\n";
-	String formatpelicula = "----------";
-	String formatpelicula2 = "\n\n----------";
-	byte[] saltolineabytes = saltolinia.getBytes(StandardCharsets.UTF_8);// Lo pasamos a bytes
-	byte[] formatpeliculabytes = formatpelicula.getBytes(StandardCharsets.UTF_8);
-	byte[] formatpeliculabytes2 = formatpelicula2.getBytes(StandardCharsets.UTF_8);
+	String saltolinea = "\n", formatpelicula = "----------", formatpelicula2 = "\n\n----------";
 	int i;
 	FileInputStream fin = null;
 	FileOutputStream fout = null;
-	BufferedWriter loggerwriter = null;
-	BufferedReader loggerreader = null;
 	//Primero, asegúrese de que ambos archivos hayan sido especificados.
 	//Copiar un Archivo
 	try {
-	    fin = new FileInputStream(rutain);
-	    fout = new FileOutputStream(rutaout, true);
-	    loggerwriter = new BufferedWriter(new FileWriter(log, true));
-	    loggerreader = new BufferedReader(new FileReader(log));
+	    fin = new FileInputStream(sc.next());
+	    fout = new FileOutputStream(sc.next(), true);
 	    //Intentar abrir los archivos
 	    menuCartelera();
-	    fout.write(formatpeliculabytes);
+	    fout.write(formatpelicula.getBytes());
 	    System.out.print(formatpelicula);
 	    do {
 		i = fin.read();
 		if (i != -1) {
 		    char x = (char) i;
-		    if (x == '#') {
-			if (ndatos == 0) {
-			    System.out.println(formatpelicula);
-			    fout.write(formatpeliculabytes);
-			}
-			System.out.print("\n" + datos[ndatos]);
-			byte[] datosbytes = datos[ndatos].getBytes();
-			fout.write(saltolineabytes);
-			fout.write(datosbytes);
-			ndatos++;
-
-		    } else if (x == '{') {
-			System.out.print(formatpelicula2);
-			fout.write(formatpeliculabytes2);
-			ndatos = 0;
-		    } else {
-			System.out.print((char) i);
-
-			fout.write(i);
+		    switch (x) {
+			case '#':
+			    if (ndatos == 0) {
+				System.out.println(formatpelicula);
+				fout.write(formatpelicula.getBytes());
+			    }
+			    System.out.print("\n" + datos[ndatos]);
+			    byte[] datosbytes = datos[ndatos].getBytes();
+			    fout.write(saltolinea.getBytes());
+			    fout.write(datosbytes);
+			    ndatos++;
+			    break;
+			case '{':
+			    System.out.print(formatpelicula2);
+			    fout.write(formatpelicula.getBytes());
+			    ndatos = 0;
+			    break;
+			default:
+			    System.out.print((char) i);
+			    fout.write(i);
+			    break;
 		    }
 		}
 	    } while (i != -1);
 	    System.out.println();
+	    imprimirlog("Se ha formateado el texto correctamente con BTB");
 	} catch (FileNotFoundException notf) {
 	    System.out.println(notf.getMessage());
+	    imprimirlog("Error: " + notf);
+	    sc.nextLine();
+	} catch (InvalidPathException invp) {
+	    System.out.println(invp.getMessage());
+	    imprimirlog("Error: " + invp);
 	} catch (IOException exc) {
 	    System.out.println("Error de E/S: " + exc);
+	    imprimirlog("Error de E/S: " + exc );
+	} catch (Exception e) {//supongo que aqui iria runtime error
+	    System.out.println("Error del usuario");
+	    imprimirlog("Error: " + e);
 	} finally {
 	    try {
 		if (fin != null) {
-		    String fecha = new SimpleDateFormat("YYYY/MM/dd").format(mydate);
-		    File log = new File("log.log");
-		    if (!log.exists()) {//nowokrks
-			loggerwriter.write((fecha + " " + time + " Se ha formateado el texto correctamente con BTB"));
-		    } else {
-			loggerwriter.write(("\n"+fecha + " " + time + " Se ha formateado el texto correctamente con BTB"));
-		    }
-
 		    fin.close();
-		    loggerwriter.close();
-		    loggerreader.close();
 		}
 	    } catch (IOException exc) {
 		System.out.println("Error al cerrar el archivo de entrada.");
+		imprimirlog("Error:"+ exc);
 	    }
 	    try {
 		if (fout != null) {
@@ -161,6 +164,7 @@ public class P07 {
 		}
 	    } catch (IOException exc) {
 		System.out.println("Error al cerrar el archivo de salida.");
+		imprimirlog("Error:"+ exc);
 	    }
 	}
     }
@@ -284,12 +288,19 @@ public class P07 {
 	writer.close();
     }
 
-    public static void imprimirlog() throws IOException {
-	File archivo = new File(log);
-	if (!archivo.exists()) {
-	}
-	BufferedReader readerlog = new BufferedReader(new FileReader(log));
-	BufferedWriter writerlog = new BufferedWriter(new FileWriter(log, true));
+    public static void imprimirlog(String error) throws IOException {
+	BufferedReader readerlog = new BufferedReader(new FileReader("log.log"));//decoración? jejs
+	BufferedWriter writerlog = new BufferedWriter(new FileWriter("log.log", true));
+	writerlog.write(obtenerFecha() + " " + error + "\n");
+	writerlog.close();
+	readerlog.close();
+    }
 
+    public static String obtenerFecha() {
+	Date mydate = new Date();
+	String fecha = new SimpleDateFormat("YYYY/MM/dd").format(mydate);
+	String hora = new SimpleDateFormat("HH:mm:ss").format(mydate);
+	String fechaCompleta = fecha + " " + hora;
+	return fechaCompleta;
     }
 }
